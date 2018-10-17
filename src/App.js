80121a799eebed0bc1a502c6c76902ae51901109
Ellen Nitchals <ellen.nitchals@gmail.com>
2018-10-16 20:04:07 -0700

    blocks move from side to side with user input

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
     super();
     this.state = {
       tetris: startingArray,
-      speed: 100,
+      speed: 600,
       go: false,
       timer: null,
       score: 0,
@@ -38,7 +38,7 @@ export default class App extends React.Component {
     const newPositions = positions.map(position => {
       return [position[0]+1, position[1]];
     })
-    const tetris = this.state.tetris.slice(0);
+    const tetris = this.state.tetris;
     positions.forEach(position => {
       tetris[position[0]].splice(position[1], 1, "00");
     });
@@ -66,6 +66,48 @@ export default class App extends React.Component {
     })
   }
 
+  moveLeft(){
+    let positions = this.getPositions();
+    const blockType = this.state.tetris[positions[0][0]][positions[0][1]];
+    const newPositions = positions.map(position => {
+      return [position[0], position[1]-1];
+    })
+    const tetris = this.state.tetris;
+    positions.forEach(position => {
+      tetris[position[0]].splice(position[1], 1, "00");
+    });
+    newPositions.forEach(position => {
+      if (tetris[position[0]+1][position[1]].slice(1) == "D"){
+        tetris[position[0]].splice(position[1], 1, blockType);
+        this.stopDrop();
+        return;
+      }
+      tetris[position[0]].splice(position[1], 1, blockType);
+    });
+    if (this.state.falling == true) this.setState({tetris});
+  }
+
+  moveRight(){
+    let positions = this.getPositions();
+    const blockType = this.state.tetris[positions[0][0]][positions[0][1]];
+    const newPositions = positions.map(position => {
+      return [position[0], position[1]+1];
+    })
+    const tetris = this.state.tetris;
+    positions.forEach(position => {
+      tetris[position[0]].splice(position[1], 1, "00");
+    });
+    newPositions.forEach(position => {
+      if (tetris[position[0]+1][position[1]].slice(1) == "D"){
+        tetris[position[0]].splice(position[1], 1, blockType);
+        this.stopDrop();
+        return;
+      }
+      tetris[position[0]].splice(position[1], 1, blockType);
+    });
+    if (this.state.falling == true) this.setState({tetris});
+  }
+
   gameOver(){
     clearInterval(this.state.timer);
     window.alert(`Game Over! \n SCORE: ${this.state.score}`);
@@ -91,6 +133,12 @@ export default class App extends React.Component {
     switch(e.keyCode){
       case 32:
         if (!this.state.go) this.go(this.state.timer);
+        break;
+      case 37://left
+        this.moveLeft();
+        break;
+      case 39://right
+        this.moveRight();
         break;
     }
   }
